90f9433592c2a7e18de96931e5ed9483fff9c522
adrian mancuso <adrian.mancuso@gmail.com>
2017-07-31 15:41:40 +1000

    append divs

--- pixart.js
+++ pixart.js
@@ -1,6 +1,11 @@
 var $brush = $('.brush');
 var	$btn = $('#set-color');
 var $input = $('#color-field');
+var $body = $('body');
+
+for (var i = 0; i < 20; i++) {
+	$body.append("<div class='square'></div>");
+}
 
 var changeColor = function() {
 	event.preventDefault();
